1a501033746f34e456821f2a2d9bae72fde71fc7
AbhishekNayak01 <abhishek.nayak@saarthi.ai>
2023-12-16 02:40:20 +0530

    changes done

---
+++ script.js
@@ -0,0 +1,389 @@
+
+KEY ="acde5f87db7487b706e8365a175de6d0"
+const API_KEY = `api_key=${KEY}`;
+const BASE_URL = 'https://api.themoviedb.org/3';
+ const API_URL = BASE_URL + '/discover/movie?sort_by=popularity.desc&'+API_KEY;
+// const API_URL = `https://api.themoviedb.org/3/movie/now_playing?${API_KEY}&language=en-US&page=1`
+const IMG_URL = 'https://image.tmdb.org/t/p/w500';
+const searchURL = BASE_URL + '/search/movie?'+API_KEY;
+
+const genres = [
+    {
+      "id": 28,
+      "name": "Action"
+    },
+    {
+      "id": 12,
+      "name": "Adventure"
+    },
+    {
+      "id": 16,
+      "name": "Animation"
+    },
+    {
+      "id": 35,
+      "name": "Comedy"
+    },
+    {
+      "id": 80,
+      "name": "Crime"
+    },
+    {
+      "id": 99,
+      "name": "Documentary"
+    },
+    {
+      "id": 18,
+      "name": "Drama"
+    },
+    {
+      "id": 10751,
+      "name": "Family"
+    },
+    {
+      "id": 14,
+      "name": "Fantasy"
+    },
+    {
+      "id": 36,
+      "name": "History"
+    },
+    {
+      "id": 27,
+      "name": "Horror"
+    },
+    {
+      "id": 10402,
+      "name": "Music"
+    },
+    {
+      "id": 9648,
+      "name": "Mystery"
+    },
+    {
+      "id": 10749,
+      "name": "Romance"
+    },
+    {
+      "id": 878,
+      "name": "Science Fiction"
+    },
+    {
+      "id": 10770,
+      "name": "TV Movie"
+    },
+    {
+      "id": 53,
+      "name": "Thriller"
+    },
+    {
+      "id": 10752,
+      "name": "War"
+    },
+    {
+      "id": 37,
+      "name": "Western"
+    }
+  ]
+
+const main = document.getElementById('main');
+const form =  document.getElementById('form');
+const search = document.getElementById('search');
+const tagsEl = document.getElementById('tags');
+
+const prev = document.getElementById('prev')
+const next = document.getElementById('next')
+const current = document.getElementById('current')
+
+var currentPage = 1;
+var nextPage = 2;
+var prevPage = 3;
+var lastUrl = '';
+var totalPages = 100;
+
+/*For showing Time slot  */
+const timeSlot =["08:00 AM" ,"11:00 AM", "01:00 PM","03:00 PM","08:00 PM","10:00 PM" ]
+
+/*Genre Option Button*/
+var selectedGenre = []
+setGenre();
+function setGenre() {
+    tagsEl.innerHTML= '';
+    genres.forEach(genre => {
+        const t = document.createElement('div');
+        t.classList.add('tag');
+        t.id=genre.id;
+        t.innerText = genre.name;
+        t.addEventListener('click', () => {
+            if(selectedGenre.length == 0){
+                selectedGenre.push(genre.id);
+            }else{
+                if(selectedGenre.includes(genre.id)){
+                    selectedGenre.forEach((id, idx) => {
+                        if(id == genre.id){
+                            selectedGenre.splice(idx, 1);
+                        }
+                    })
+                }else{
+                    selectedGenre.push(genre.id);
+                }
+            }
+            console.log(selectedGenre)
+            getMovies(API_URL + '&with_genres='+encodeURI(selectedGenre.join(',')))
+            highlightSelection()
+        })
+        tagsEl.append(t);
+    })
+}
+
+/**highlight Selection Button Genre */
+function highlightSelection() {
+    const tags = document.querySelectorAll('.tag');
+    tags.forEach(tag => {
+        tag.classList.remove('highlight')
+    })
+    clearBtn()
+    if(selectedGenre.length !=0){   
+        selectedGenre.forEach(id => {
+            const hightlightedTag = document.getElementById(id);
+            hightlightedTag.classList.add('highlight');
+        })
+    }
+
+}
+
+function clearBtn(){
+    let clearBtn = document.getElementById('clear');
+    if(clearBtn){
+        clearBtn.classList.add('highlight')
+    }else{
+            
+        let clear = document.createElement('div');
+        clear.classList.add('tag','highlight');
+        clear.id = 'clear';
+        clear.innerText = 'Clear x';
+        clear.addEventListener('click', () => {
+            selectedGenre = [];
+            setGenre();            
+            getMovies(API_URL);
+        })
+        tagsEl.append(clear);
+    }
+    
+}
+/*** end Genre button***/
+
+/**Get Movie List */
+getMovies(API_URL);
+
+
+function getMovies(url) {
+  lastUrl = url;
+    fetch(url).then(res => res.json()).then(data => {
+        console.log(data.results)
+        if(data.results.length !== 0){
+            showMovies(data.results);
+            currentPage = data.page;
+            nextPage = currentPage + 1;
+            prevPage = currentPage - 1;
+            totalPages = data.total_pages;
+
+            current.innerText = currentPage;
+
+            if(currentPage <= 1){
+              prev.classList.add('disabled');
+              next.classList.remove('disabled')
+            }else if(currentPage>= totalPages){
+              prev.classList.remove('disabled');
+              next.classList.add('disabled')
+            }else{
+              prev.classList.remove('disabled');
+              next.classList.remove('disabled')
+            }
+
+            tagsEl.scrollIntoView({behavior : 'smooth'})
+
+        }else{
+            main.innerHTML= `<h1 class="no-results">No Results Found</h1>`
+        }
+       
+    })
+
+}
+
+/** Showing  Card**/
+function showMovies(data) {
+    main.innerHTML = '';
+
+    data.forEach(movie => {
+        const {title, poster_path, vote_average, overview, id} = movie;
+        console.log("movie" , movie)
+     
+        const movieEl = document.createElement('div');
+        movieEl.classList.add('movie');
+        movieEl.id = id
+        movieEl.innerHTML = `
+             <img src="${poster_path? IMG_URL+poster_path: "http://via.placeholder.com/1080x1580" }" alt="${title}">
+            <div class="movie-info">
+                <h3>${title}</h3>
+                <span class="${getColor(vote_average)}">${vote_average}</span>
+            </div>
+
+        `
+
+        main.appendChild(movieEl);
+
+        document.getElementById(id).addEventListener('click', () => {
+          console.log(id)
+          openNav(movie)
+        })
+    })
+}
+
+/****End Movie List ****/
+
+/****Overlay Modal For Showing Details ****/
+const overlayContent = document.getElementById('overlay-content');
+/* Open when someone clicks on the span element */
+function openNav(movie) {
+  let id = movie.id;
+  fetch(BASE_URL + '/movie/'+id+'/videos?'+API_KEY).then(res => res.json()).then(videoData => {
+    console.log(videoData);
+    if(videoData){
+      document.getElementById("myNav").style.width = "100%";
+      if(videoData.results.length > 0){
+        var embed = [];
+        videoData.results.forEach((video, idx) => {
+          let {name, key, site} = video
+
+          if(site == 'YouTube' && name?.includes("Official Trailer")){
+              
+            embed.push(`
+              <iframe width="560" height="315" src="https://www.youtube.com/embed/${key}" title="${name}" class="embed hide" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+          
+          `)
+          }
+        })
+        const btn =  timeSlot.map((data , index)=> {
+          return  `<button key =${index} class ="time-btn">${data}</button>`
+        })
+        console.log(btn)
+        var content = `
+        <div class ="trailer"> 
+        ${embed.join('')}
+        </div>
+        <div class ="details">
+        <h1 class="no-results">${movie.original_title}</h1>
+        <h3 class="no-results">Release Date : ${movie.release_date }</h3>
+         <p class="no-results">
+         ${movie.overview}
+         </p>
+
+         <h3 class="no-results">Rating: ${movie.vote_average?.toFixed(2)}/10</h3>
+          
+         <div class ="time-btn-cotainer">
+          ${btn.join("")}
+         </div>
+       
+
+        </div>
+        `
+        overlayContent.innerHTML = content;
+        activeSlide=0;
+        showVideos();
+      }else{
+        overlayContent.innerHTML = `<h1 class="no-results">No Results Found</h1>`
+      }
+    }
+  })
+}
+
+/* Close when someone clicks on the "x" symbol inside the overlay */
+function closeNav() {
+  document.getElementById("myNav").style.width = "0%";
+}
+
+var activeSlide = 0;
+var totalVideos = 0;
+
+function showVideos(){
+  let embedClasses = document.querySelectorAll('.embed');
+
+
+  totalVideos = embedClasses.length; 
+  embedClasses.forEach((embedTag, idx) => {
+    if(activeSlide == idx){
+      embedTag.classList.add('show')
+      embedTag.classList.remove('hide')
+
+    }else{
+      embedTag.classList.add('hide');
+      embedTag.classList.remove('show')
+    }
+  })
+
+}
+
+/****End Overlay Modal For Showing Details ****/
+
+/**Rating color  **/
+function getColor(vote) {
+    if(vote>= 8){
+        return 'green'
+    }else if(vote >= 5){
+        return "orange"
+    }else{
+        return 'red'
+    }
+}
+/**End Rating color  **/
+
+/**On Submit Search Movie  **/
+form.addEventListener('submit', (e) => {
+    e.preventDefault();
+    const searchTerm = search.value;
+    selectedGenre=[];
+    setGenre();
+    if(searchTerm) {
+        getMovies(searchURL+'&query='+searchTerm)
+    }else{
+        getMovies(API_URL);
+    }
+
+})
+
+/**End On Submit Search Movie  **/
+
+
+/**On Click Next Page Movie List **/
+
+prev.addEventListener('click', () => {
+  if(prevPage > 0){
+    pageCall(prevPage);
+  }
+})
+
+next.addEventListener('click', () => {
+  if(nextPage <= totalPages){
+    pageCall(nextPage);
+  }
+})
+
+function pageCall(page){
+  let urlSplit = lastUrl.split('?');
+  let queryParams = urlSplit[1].split('&');
+  let key = queryParams[queryParams.length -1].split('=');
+  if(key[0] != 'page'){
+    let url = lastUrl + '&page='+page
+    getMovies(url);
+  }else{
+    key[1] = page.toString();
+    let a = key.join('=');
+    queryParams[queryParams.length -1] = a;
+    let b = queryParams.join('&');
+    let url = urlSplit[0] +'?'+ b
+    getMovies(url);
+  }
+}
+/**End On Click Next Page Movie List **/
+
